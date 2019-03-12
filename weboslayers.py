02691f79933b3ceafeea353dbf4d69cb766a5f5f
Heegoo Han <heegoo.han@lge.com>
2019-03-12 15:20:51 +0900

    try mesa build
    make mesa will succeed but
    make webos-image fails like something below:
    ----
    ERROR: Nothing PROVIDES 'nativesdk-mesa' (but virtual:nativesdk:/home/heegoo/build-webos/meta-webosose/meta-webos/recipes-graphics/virglrenderer/virglrenderer_git.bb DEPENDS on or otherwise requires it). Close matches:
      nativesdk-make
      nativesdk-m4
      nativesdk-gtest
    ----

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['raspberrypi3','qemux86']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.32,commit=0193e1f', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=morty,commit=4b32784', 'oe-core'),
+('meta',                       5, 'git://github.com/webosose-emulator/openembedded-core.git',    'branch=morty-webos-emulator,commit=ee8ea05', 'oe-core'),
 
 ('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=morty,commit=997caf9', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
@@ -70,16 +70,16 @@ webos_layers = [
 
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=krogoth,commit=dcfcb58', ''),
 
-('meta-webos-backports-2.3',  30, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-webos-backports-2.4',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-webos-backports-2.5',  32, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-webos-backports-2.6',  33, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-webos-backports-2.3',  30, 'git://github.com/webosose-emulator/meta-webosose.git',            '', ''),
+('meta-webos-backports-2.4',  31, 'git://github.com/webosose-emulator/meta-webosose.git',            '', ''),
+('meta-webos-backports-2.5',  32, 'git://github.com/webosose-emulator/meta-webosose.git',            '', ''),
+('meta-webos-backports-2.6',  33, 'git://github.com/webosose-emulator/meta-webosose.git',            '', ''),
 
 ('meta-ros2',                 38, 'git://github.com/lgsvl/meta-ros2.git',                   'branch=ros2pr,commit=b92ab96',''),
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=b8dcca1', ''),
+('meta-webos',                40, 'git://github.com/webosose-emulator/meta-webosose.git',            'branch=webos-emulator,commit=9f091f4', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=morty,commit=2a19226', ''),
-('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-webos-raspberrypi',    51, 'git://github.com/webosose-emulator/meta-webosose.git',            '', ''),
 
-('meta-webos-ros2',           52, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-webos-ros2',           52, 'git://github.com/webosose-emulator/meta-webosose.git',            '', ''),
 ]
